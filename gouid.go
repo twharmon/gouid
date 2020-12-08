a450465f5b05430c9f7a5ef5f3444f33e069a652
Travis Harmon <twharmon@gmail.com>
2020-12-08 13:28:33 -0600

    Add ByteArray type

--- gouid.go
+++ gouid.go
@@ -10,6 +10,9 @@ import (
 // GOUID is a byte slice.
 type GOUID []byte
 
+// GOUID16 is a byte array with 16 bytes.
+type GOUID16 [16]byte
+
 var alphabet = []byte("abcdefghijklmnopqrstuvwxyz0123456789")
 
 // String returns a string with the given size. Ids are
@@ -31,6 +34,13 @@ func Bytes(size int) GOUID {
 	return b
 }
 
+// ByteArr16 returns 16 cryptographically secure random bytes.
+func ByteArr16() GOUID16 {
+	var ba [16]byte
+	rand.Read(ba[:])
+	return ba
+}
+
 // MarshalJSON hex encodes the gouid.
 func (g GOUID) MarshalJSON() ([]byte, error) {
 	return json.Marshal(hex.EncodeToString(g))
@@ -43,6 +53,25 @@ func (g *GOUID) UnmarshalJSON(data []byte) error {
 	if err == nil {
 		str, e := hex.DecodeString(x)
 		*g = GOUID([]byte(str))
+		err = e
+	}
+	return err
+}
+
+// MarshalJSON hex encodes the gouid.
+func (g GOUID16) MarshalJSON() ([]byte, error) {
+	return json.Marshal(hex.EncodeToString(g[:]))
+}
+
+// UnmarshalJSON decodes a hex encoded string into a gouid.
+func (g *GOUID16) UnmarshalJSON(data []byte) error {
+	var x string
+	err := json.Unmarshal(data, &x)
+	if err == nil {
+		str, e := hex.DecodeString(x)
+		var ba [16]byte
+		copy(ba[:], []byte(str))
+		*g = ba
 		err = e
 	}
 	return err
